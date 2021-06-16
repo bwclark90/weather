bef0da4f8c8d478f8d8d84f9bea194105cbb72a6
Brandon Clark <clarkwbrandon2@gmail.com>
2021-06-15 19:15:17 -0700

    local storage working, styling complete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 
-
+//short date function for displaying date
 
 const shortDate = (dt) => {
   let dateObject = new Date(dt * 1000)
@@ -12,9 +12,10 @@ const shortDate = (dt) => {
 
 
 
-document.getElementById('searchBtn').addEventListener('click', event => {
+
+const mainFunction = event => {
   event.preventDefault()
-//5 day forcast
+
   
   
 
@@ -23,66 +24,103 @@ document.getElementById('searchBtn').addEventListener('click', event => {
 
 axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('search').value}&units=imperial&appid=7d550b5d9d3f2ac58186726ddd1ecd49`)
     .then(res => {
+      let storage = JSON.parse(localStorage.getItem('history')) || []
+      if (storage.indexOf(res.data.name)=== -1){
+        storage.push(res.data.name)
+        localStorage.setItem('history', JSON.stringify(storage)) 
+
+      }
       const weather = res.data
       console.log(weather)
       const lon = weather.coord.lon
       const lat = weather.coord.lat
 
-      document.getElementById('date').innerHTML = `${weather.dt}`
+      document.getElementById('date').innerHTML = `${shortDate(weather.dt)}`
       document.getElementById('cityLocation').innerHTML = `${weather.name}`
       document.getElementById('conditions').innerHTML = `<img src='http://openweathermap.org/img/wn/${weather.weather[0].icon}.png'> ${weather.weather[0].description}`
       document.getElementById('temp').innerHTML = `${weather.main.temp}`
       document.getElementById('hum').innerHTML = `${weather.main.humidity}`
       document.getElementById('wndSpd').innerHTML = `${weather.wind.speed}`
       
-
+//5 day forcast loop
       axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=7d550b5d9d3f2ac58186726ddd1ecd49`)
         .then(res => {
           const forcast = res.data
           console.log(forcast)
 
-          for (let i = 1; i < 5; i++) {
-
-
+          for (let i = 1; i < 6; i++) {
+              
             let forcastElem = document.createElement('div')
             forcastElem.className = 'forcastcol'
+            console.log(forcast.daily[i].dt)
             forcastElem.innerHTML = `
     <div class="card" style="width: 18rem;">
     <div class="card-body">
     <h5 class="card-title">${shortDate(forcast.daily[i].dt)}</h5>
-    <h5 class="card-title">${forcast.daily[i].dt_txt}</h5>
+    
     <img src = 'http://openweathermap.org/img/wn/${forcast.daily[i].weather[0].icon}.png'> 
     <h6 class="card-subtitle mb-2 text-muted">Max Temp: ${forcast.daily[i].temp.max}&degF</h6>
     <h6 class="card-text">Min Temp: ${forcast.daily[i].temp.min}&degF</h6>
     <h6 class="card-text">Humidity: ${forcast.daily[i].humidity}%</h6>
     <h6 class="card-text">UV index: ${forcast.daily[i].uvi}</h6>
+    <button type="button" id="uvInd" class="btn btn-primary">UV<span class="badge"></span></button>
+    
     
   </div>
 </div>
      
 `
-            document.getElementById('5dayforcast').append(forcastElem)
+            
+            
+            document.getElementById('row2').append(forcastElem)
           }
-
+          //uv index badges change color accordingly
+          if (forcast.daily.uvi >= 0 && forcast.daily.uvi < 3) {
+            document.getElementById('uvInd').classList.add('bg-success')
+          } else if (forcast.daily.uvi >= 3 && forcast.daily.uvi < 6) {
+            document.getElementById('uvInd').classList.add('bg-warning')
+          } else if (forcast.daily.uvi >= 6 && forcast.daily.uvi < 9) {
+            document.getElementById('uvInd').classList.add('bg-danger')
+          } else {
+            document.getElementById('uvInd').classList.add('bg-dark')
+          }
         })
         .catch(err => console.error(err))
       
 })
   .catch(err => console.error(err))
   document.getElementById('search').value = ''
+}
+document.getElementById('searchBtn').addEventListener('click', mainFunction)
+
+//local storage
+let history = JSON.parse(localStorage.getItem('history')) || []
+
+document.getElementById('historyBtn').addEventListener('click', event => {
+  event.preventDefault()
+  let storage = JSON.parse(localStorage.getItem('history')) || []
+  document.getElementById('searchHistory').innerHTML= ''
+  storage.forEach(item =>{
+    let li = document.createElement('li')
+    li.textContent = item
+    li.onclick = mainFunction
+    document.getElementById('searchHistory').append(li)
+  })
+})
+// clear history
+document.getElementById('clearHistory').addEventListener('click', event => {
+  event.preventDefault()
+  const clrHst = confirm('Are you sure you want to clear your history?')
+  if (clrHst) {
+    localStorage.removeItem('history')
+    history = []
+  }
 })
 
 
 
 
-// document.getElementById('searchBtn').addEventListener('click', event => {
-//   event.preventDefault()
-//   axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${document.getElementById('search').value}&appid=7d550b5d9d3f2ac58186726ddd1ecd49`)
-//   .then(res =>{
-//   const forcast = res.data
-//   console.log(forcast)
-// })
 
 
-// })
+
 
